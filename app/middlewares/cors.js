96b18a5b13290b3bd58779aab88243ccce7113ef
Agustin-Gonzalorena <agonzalorena@hotmail.com>
2024-01-10 20:14:42 -0300

    Update accepted origins in corsMiddleware

--- app/middlewares/cors.js
+++ app/middlewares/cors.js
@@ -1,15 +1,15 @@
 import cors from "cors";
 
 const ACCEPTED_ORIGINS = [
-  "http://localhost:8080",
+  "http://localhost:5174",
   "http://localhost:1234",
   "http://localhost:3000",
   "https://movies.com",
 ];
 
-export const corsMiddleware = ({ acceptedOrigins = "*" } = {}) =>
+export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) =>
   cors({
-    origin: (origin, callback) => {
+    origin: true /* (origin, callback) => {
       if (acceptedOrigins.includes(origin)) {
         return callback(null, true);
       }
@@ -19,5 +19,5 @@ export const corsMiddleware = ({ acceptedOrigins = "*" } = {}) =>
       }
 
       return callback(new Error("Not allowed by CORS"));
-    },
+    }, */,
   });
